29495e31e9a46bce31dd0ab64b7b7f553a8a8451
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-06-11 11:42:03 +0530

    Update rep.py

--- rep.py
+++ rep.py
@@ -1,6 +1,11 @@
 n = int(input())
 l=list(map(int,input().split()))
+k=0
 for i in range(n):
 	if l.count(l[i])>1:
 		print(l[i],end="")
 		break;
+	else :
+		k=k+1
+if k==n :
+	print("unique",end="")
