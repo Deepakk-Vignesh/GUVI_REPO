ce7c62b32d2d46c788a2f0d9e015912603490e31
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-06-11 09:44:28 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,12 +1,15 @@
+
 #include<stdio.h>
+#include<string.h>
 int main(){
   char a;
   scanf("%c",&a);
-  if(a<65&&a>90&&a>122&&a<97)
+  if(!((a>=65&&a<=90)||(a<=122&&a>=97)))
   printf("invalid");
-  else
+  else{
   if(tolower(a)=='a'||tolower(a)=='e'||tolower(a)=='i'||tolower(a)=='o'||tolower(a)=='u')
   printf("Vowel");
   else
   printf("Consonant");
+  }
 }
