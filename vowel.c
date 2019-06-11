1323c19c2a4b38cf49ccfc692b68554378e61ed4
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-06-11 09:35:12 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  char a;
+  scanf("%c",&a);
+  if((tolower(a)<65&&tolower(a)>90))
+  printf("invalid);
+  else
+  if(tolower(a)=='a'||tolower(a)=='e'||tolower(a)=='i'||tolower(a)=='o'||tolower(a)=='u')
+  printf("Vowel");
+  else
+  printf("Consonant");
+}
