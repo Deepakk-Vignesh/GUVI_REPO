4d190813971d824dec6b9a44eea02e2ceffefe66
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-06-11 09:40:23 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,7 +2,7 @@
 int main(){
   char a;
   scanf("%c",&a);
-  if((a<65&&a>90)&&(a>122&&a<97))
+  if(a<65&&a>90&&a>122&&a<97)
   printf("invalid");
   else
   if(tolower(a)=='a'||tolower(a)=='e'||tolower(a)=='i'||tolower(a)=='o'||tolower(a)=='u')
