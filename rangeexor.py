1cefff5860b8401f6f0b910a6f42236e7d8cd09b
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-06-27 19:08:48 +0530

    Create rangeexor.py

---
+++ rangeexor.py
@@ -0,0 +1,8 @@
+n,q=map(int,input().split())
+b=list(map(int,input().split()))
+for x in range (q):
+    s,t=map(int,input().split())
+    y=0
+    for a in range(s-1,t):
+        y=y^b[a]
+    print(y)
