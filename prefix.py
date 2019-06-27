f7130584f07286e70f7dfb590450bdc3b169d1d7
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-06-27 17:03:57 +0530

    Update prefix.py

--- prefix.py
+++ prefix.py
@@ -1,3 +1,11 @@
-a=int(input)
-b=list(map(input()).split)
-print(b)
+a=int(input())
+b=[]
+for x in range(a):
+    b.insert(x,input())
+b=sorted(b)
+i=0
+s1=list(b[0])
+s2=list(b[a-1])
+while i<len(s1) and s1[i]==s2[i]:
+    print(s1[i],end="")
+    i+=1
