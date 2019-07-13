a522cca905489e68dc512585a7c54e987b99e8d8
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-07-13 08:55:04 +0530

    Update strbalance.py

--- strbalance.py
+++ strbalance.py
@@ -1,12 +1,16 @@
 a=input()
 b=input()
 c=a.index('|')
-d=len(a)
-if d%2==1 or c!=d//2-1 and c!=d//2:
+d=len(a)+len(b)
+if d%2==0:
     print("Impossible")
 else:
-    if c==d//2-1:
+    if c<=d//2-1:
         a=b+a
     else:
         a=a+b
-    print(a)
+    c=a.index('|')
+    if c!=d//2-1 and c!=d//2:
+        print("Impossible")
+    else:
+        print(a)
