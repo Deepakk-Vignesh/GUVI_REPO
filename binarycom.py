2e3e1acbfef9f35377a082d5b4266f0d183b45d3
Deepakk-Vignesh <34776909+Deepakk-Vignesh@users.noreply.github.com>
2019-07-13 09:34:53 +0530

    Update binarycom.py

--- binarycom.py
+++ binarycom.py
@@ -1,6 +1,11 @@
+def f(l):
+    l.sort(key=lambda n:bin(n).count('1'))
+    return l
 a=int(input())
 b=2**a
-for n in range(b):
+d=[i for i in range(b)]
+d=f(d)
+for n in d:
     c=bin(n).replace("0b","")
     for i in range(a-len(str(c))):
         print(0,end="")
